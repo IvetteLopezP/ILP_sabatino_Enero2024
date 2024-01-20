ee2e7d4f2a54e15ffe931f01367dc376f631cfe5
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-01-20 12:05:25 -0600

    Ejemplo saludo

---
+++ Saludo.py
@@ -0,0 +1,9 @@
+# Saludo
+# Comentarios.- Sirven para detallar o documentar nuestro código o programa
+# A los comentarios no los toma en cuenta python
+print(5+5) # Suma
+print(6-2); # Resta
+print(7*8) # Multiplicación
+print(20/5) # Resta
+print("Hola Mundo") #Saludo
+print('Ivette') # Nombre
