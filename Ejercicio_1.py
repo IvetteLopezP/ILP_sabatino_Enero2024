d9e607a241c34067b71e924c1e485edc9bd24316
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-01-27 11:45:00 -0600

    Update Ejercicio_1.py

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -9,10 +9,12 @@ import math
 calificación_1 = float(input("Primer parcial:"))
 calificación_2 = float(input("Segundo parcial:"))
 calificación_3 = float(input("Tercer parcial:"))
-#Procesos (Cálculos y operaciones matemáticas)
 
+#Procesos (Cálculos y operaciones matemáticas)
 promedio = (calificación_1 + calificación_2 + calificación_3)/3
+calificación_final = (if("El promedio de 3 calificaciones =", >=6,"Aprobado",if("El promedio de 3 calificaciones =",<=10)"Reprobado"))
 
 #Salida de datos
 print("El promedio de 3 calificaciones =", round(promedio,1))
+print("Calificación Final =",calificación_final)
 
