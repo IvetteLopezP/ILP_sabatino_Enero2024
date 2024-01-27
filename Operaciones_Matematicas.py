64fadaa74ca8af161034026a0330fb3403873e98
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-01-27 08:23:32 -0600

    Update Operaciones_Matematicas.py

---
+++ Operaciones_Matematicas.py
@@ -0,0 +1,17 @@
+#Opereciones matemáticas
+#Entrada de datos
+#Declarar, crear instancias variables o constantes
+numero_1 = 10
+numero_2 = 2.5
+
+#Procesos (Cálculos y operaciones matemáticas)
+suma = numero_1 + numero_2
+resta = numero_1 - numero_2
+multiplicación = numero_1 * numero_2
+división = numero_1 / numero_2
+
+#Salida de datos
+print("La suma es =", suma)
+print('La resta es =', resta)
+print("La multiplicación es =", multiplicación)
+print("La división es =", división)
