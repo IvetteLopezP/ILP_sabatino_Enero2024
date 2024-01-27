4aedcc26a986ea51b04531e32672b1dd8671e73b
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-01-27 12:11:38 -0600

    Create Ejercicio_3.py

---
+++ Ejercicio_3.py
@@ -0,0 +1,17 @@
+#Ejercicio 3
+
+#importar librerias o blibliotecas
+import math
+#Entrada de datos
+#Determinar la edad de una persona conociendo el año actual y el año de nacimiento.
+#Declarar, crear instancias variables o constantes
+
+año_nacimiento = float(input("Ingresa un número:")) 
+fecha_actual = float(input("Ingresa otro número:"))
+
+#Procesos (Cálculos y operaciones matemáticas)
+
+edad = fecha_actual - año_nacimiento
+
+#Salida de datos
+print("Edad", round(edad, 1))
