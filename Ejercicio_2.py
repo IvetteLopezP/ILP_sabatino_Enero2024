81686ae915f5ada4c6f490e788942cf9296f1b44
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-01-27 12:03:31 -0600

    Create Ejercicio_2.py

---
+++ Ejercicio_2.py
@@ -0,0 +1,19 @@
+#Opereciones matemáticas
+
+#Calcular el área de un circulo
+#Entrada de datos
+#Fórmula (Pi*radio)**2
+#Declarar, crear instancias variables o constantes
+
+radio = float(input("Ingresa un número:")) 
+
+#Declarar constante
+PI = 3.1416
+
+#Procesos (Cálculos y operaciones matemáticas)
+
+área = (PI * radio) ** 2
+
+
+#Salida de datos
+print("El área del circulo es", round(área,1)
