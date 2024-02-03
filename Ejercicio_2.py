e5f3073ef3665aa4b2902124530317f96b14d0ed
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-02-03 11:28:58 -0600

    Update Ejercicio_2.py

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -6,14 +6,15 @@
 #Declarar, crear instancias variables o constantes
 
 radio = float(input("Ingresa un número:")) 
-
+diámetro = float(input("ingresa un número"))
 #Declarar constante
 PI = 3.1416
 
 #Procesos (Cálculos y operaciones matemáticas)
 
 área = (PI * radio) ** 2
-
+perímetro = (PI * diámetro)
 
 #Salida de datos
-print("El área del circulo es", round(área,1)
+print("El área del circulo es", round(área,1))
+print("El perimetro de un circulo es", round(perímetro,1))
