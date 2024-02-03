37094eecefe06e5fd0cc721285ec64e3637578ae
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-02-03 11:28:06 -0600

    Update Ejercicio_1.py
    Clasificación: aprobado y reprobado.

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -4,17 +4,23 @@
 import math
 #Entrada de datos
 
-# Calcular el promedio de 3 calificaciones y decir si es aprobado o reprobado.
-
 calificación_1 = float(input("Primer parcial:"))
 calificación_2 = float(input("Segundo parcial:"))
 calificación_3 = float(input("Tercer parcial:"))
 
+# Calcular el promedio de 3 calificaciones y decir si es aprobado o reprobado.
 #Procesos (Cálculos y operaciones matemáticas)
+
 promedio = (calificación_1 + calificación_2 + calificación_3)/3
-calificación_final = (if("El promedio de 3 calificaciones =", >6,"Aprobado","El promedio de 3 calificaciones =",<6,"Reprobado"))
 
 #Salida de datos
 print("El promedio de 3 calificaciones =", round(promedio,1))
-print("Calificación Final =",calificación_final)
+
+if (promedio >= 6 and promedio <=10):
+   print("aprobado") 
+elif (promedio <=6 and promedio >=0):
+    print( "reprobado)")
+elif (promedio <0 or promedio >10):
+    print("Error")
+
 
