af2be22a087d74bd1903a7383ee5bedc61e0d213
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-02-10 09:59:33 -0600

    Create Ejercicio_7.py

---
+++ Ejercicio_7.py
@@ -0,0 +1,25 @@
+#Ejericicio 7
+ #Pedir el nivel del agua en metros de una cisterna
+
+#Declarar variables o constantes
+
+#Entrada de Datos
+nivel_de_agua= float(input("nivel de agua en metros:")) 
+
+#Procesos (Cálculos y operaciones matemáticas)
+
+
+if (nivel_de_agua > 6 ):     
+    print("Desbordamiento de agua en cisterna")
+elif (nivel_de_agua = 6 ):
+     print("Apagar bomba")
+elif (nivel_de_agua = 5.9 and nivel_de_agua >= 4):     
+    print("Desacelerar bomba")
+elif (nivel_de_agua =)
+
+
+
+#Salida
+
+print("Es un número par:", par)
+print("Es un número impar:", impar)
