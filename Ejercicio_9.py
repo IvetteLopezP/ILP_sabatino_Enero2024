ed1e19f6d200fbe928052ef89f0f311f0b1fecca
IvetteLopezP <157227330+IvetteLopezP@users.noreply.github.com>
2024-02-12 23:56:43 -0600

    Update Ejercicio_9.py

--- Ejercicio_9.py
+++ Ejercicio_9.py
@@ -33,6 +33,68 @@ else:
   print("Opción no válida")
 
 #b) Calcular la nómina de un empleado en ENERO del 2024
+
+#Declarar variables o constantes
+
+#Entrada de Datos
+
+días_laborados = 31
+pago_día = 280
+iva =  0.16
+isr =  0.18
+#Procesos (Cálculos y operaciones matemáticas)
+
+pago_base = (días_laborados * pago_día)
+iva_trasladado = (pago_base * iva)
+subtotal= (pago_base + iva_trasladado)
+iva_retenido = (iva_trasladado * 2/3)
+isr_retenido = (pago_base * isr)
+pago_neto = (subtotal - iva_retenido) - (isr_retenido)
+
+#Salida
+
+def mostrar_pago_base():
+    print("El pago base es =", round(pago_base_calculado, 2))
+
+def mostrar_iva_trasladado():
+    print("El IVA trasladado es =", round(iva_trasladado_calculado, 2))
+
+def mostrar_subtotal():
+    print("El subtotal es =", round(subtotal_calculado, 2))
+
+def mostrar_iva_retenido():
+    print("El IVA retenido es =", round(iva_retenido_calculado, 2))
+
+def mostrar_isr_retenido():
+    print("El ISR retenido es =", round(isr_retenido_calculado, 2))
+
+def mostrar_pago_neto():
+    print("El pago neto es =", round(pago_neto_calculado, 2))
+
+print("*********** MENÚ DE OPCIONES / MÓDULOS ***************")
+print(" 1. pago base")
+print(" 2. IVA trasladado")
+print(" 3. Subtotal")
+print(" 4. IVA retenido")
+print(" 5. ISR retenido")
+print(" 6. Pago nómina enero 2024")
+opción = int(input("Ingresa una opción: " ))
+  
+if opción == 1:
+    mostrar_pago_base()
+elif opción == 2:
+    mostrar_iva_trasladado()
+elif opción == 3:
+    mostrar_subtotal()
+elif opción == 4:
+    mostrar_iva_retenido()
+elif opción == 5:
+    mostrar_isr_retenido()
+elif opción == 6:
+    mostrar_pago_neto()
+else:
+  print("Opción no válida")
+
 #c) Generar una contraseña con el número de caracteres pedidos menor o igual a 5, si es mayor que 5 mostrar mensaje de error
 #d) Pedir al usuario su nombre, primer ap., segundo ap. y edad e imprimir un saludo con sus datos
 
